7d6e737509fbe2eaeefba55035a937dab1301a75
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-08 19:00:33 +0300

    crating and editing js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const mainElement = document.querySelector("main")
+
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Your Header Text Here';
+newHeader.id = 'victory';
+newHeader.textContent = 'levy is the champion';
+document.body.appendChild(newHeader);
+
+
+
+
